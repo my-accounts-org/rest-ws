ecbe9c04fb3c6af85375248014d7e58ed2096ff0
pandav14 <viva17j@gmail.com>
2019-07-27 18:28:49 +0530

    jwt

--- src/main/java/com/company/ac/resources/GroupResource.java
+++ src/main/java/com/company/ac/resources/GroupResource.java
@@ -5,18 +5,13 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import com.company.ac.models.Group;
-import com.company.ac.models.User;
-import com.company.ac.models.company.Company;
 import com.company.ac.services.GroupService;
 import com.company.ac.services.impl.GroupServiceImpl;
 
